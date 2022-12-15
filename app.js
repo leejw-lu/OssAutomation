bfbc35484a95950dc1de6db9f9e1a769a27fa1ba
jiwooLee <61766218+leejw-lu@users.noreply.github.com>
2022-12-15 14:53:31 +0900

    [Style] delete unnecessary lines

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const multer = require("multer");
 const port = 3000;
 
 app.set("view engine", "ejs");
-
 app.use(
   bodyParser.urlencoded({
     extended: false,
@@ -16,7 +15,6 @@ app.use(
 );
 app.use(express.json());
 app.use('/public', express.static(__dirname + '/public'));
-
 
 //로그인 후 세션 유지를 위한 코드
 app.use(
